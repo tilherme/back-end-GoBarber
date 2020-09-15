91e84dc26e373cd0c1dbd4c0a2cbc7adf2206572
guilherme <gui.mauricio@hotmail.com.br>
2020-09-14 23:14:50 -0300

    listagem feita

--- src/routes/appointment.routes.ts
+++ src/routes/appointment.routes.ts
@@ -1,21 +1,33 @@
-import {Router} from 'express';
-import { uuid } from 'uuidv4';
+import{Router} from 'express';
+
 import {startOfHour, parseISO } from 'date-fns';
 
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
 const appointmentRouter = Router();
-const appointments = [];
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentRouter.get('/', (request, response)=>{
+    const appointments = appointmentsRepository.all();
+
+    return response.json(appointments);
+})
+
+
 
 appointmentRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
     const parseDate = startOfHour(parseISO(date));
 
-    const appointment = {
-        id: uuid(),
-        provider,
-        date:parseDate,
-    }
-    appointments.push(appointment);
+    const findAppointmentInSameDate = appointmentsRepository.findByDate(parseDate)
+        if(findAppointmentInSameDate){
+            return response.status(400).json({mensage: 'This appointment is already booked'})
+        }
+
+    const appointment = appointmentsRepository.create(provider, parseDate)
+
+
  return response.json({ appointment});  
 })
 
